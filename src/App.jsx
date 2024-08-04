4c4955dd6105124c848ce49f06d3920eeb35ffe8
JoshLikesToCode <jstone925314@gmail.com>
2024-08-03 23:49:13 -0700

    Adding React Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Monkey" animal="Cat" breed="Russian Blue" />
-    <Pet name="Tinker" animal="Cat" breed="Long haired Monkey" />
-    <Pet name="Willow" animal="Dog" breed="Mini Aussie" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
